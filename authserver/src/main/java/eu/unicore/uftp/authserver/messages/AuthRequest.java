6c09e14a14c68c4f7304740a5b0b7cfd28d12af8
Bernd Schuller <b.schuller@fz-juelich.de>
2020-09-21 12:54:05 +0200

    use proper version numbers for release

--- authserver/src/main/java/eu/unicore/uftp/authserver/messages/AuthRequest.java
+++ authserver/src/main/java/eu/unicore/uftp/authserver/messages/AuthRequest.java
@@ -6,12 +6,13 @@ package eu.unicore.uftp.authserver.messages;
 public class AuthRequest {
 	public String serverPath;
 	public int streamCount = 1;
-	public boolean send = true;
 	public boolean append = false;
 	public boolean compress = false;
 	public String encryptionKey = null;  
 	public String group = null;
 	public String client = null;
-	public boolean sessionMode = false;
 	public boolean persistent = true;
+	
+	public boolean sessionMode = false;
+	public boolean send = true;
 }
